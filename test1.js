bcd182751933b532164ff53350449597bbe880e8
SauloRdz <saulo.rdz@me.com>
2021-03-04 11:48:42 -0600

    subimos ya un proyecto

--- test1.js
+++ test1.js
@@ -1,1 +1,10 @@
-
+//vamos a hacer los cambios
+const { Controller } = require("ethernet-ip");
+ 
+const PLC = new Controller();
+ 
+// Controller.connect(IP_ADDR[, SLOT])
+// NOTE: SLOT = 0 (default) - 0 if CompactLogix
+PLC.connect("172.0.0.1", 0).then(() => {
+    console.log(PLC.properties);
+});
